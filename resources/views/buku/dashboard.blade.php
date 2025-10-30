470d5d09f9b159d5dc95d17f9b793f14cfa58144
Red-1412 <dzikra24ti@mahasiswa.pcr.ac.id>
2025-10-30 09:44:12 +0700

    kuis1

--- resources/views/buku/dashboard.blade.php
+++ resources/views/buku/dashboard.blade.php
@@ -56,71 +56,17 @@
                 Interface
             </div>
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="{{ asset('assets-buku/css/sb-admin-2.css') }}" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">Buttons</a>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">Cards</a>
-                    </div>
-                </div>
-            </li>
-
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="{{ asset('assets-buku/css/sb-admin-2.css') }}" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Buku</span>
                 </a>
-                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">Colors</a>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">Borders</a>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">Animations</a>
-                        <a class="collapse-item" href="{{ asset('assets-buku/css/sb-admin-2.css') }}l">Other</a>
-                    </div>
-                </div>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Addons
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="{{ asset('assets-buku/css/sb-admin-2.css') }}" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Pages</span>
-                </a>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ asset('assets-buku/css/sb-admin-2.css') }}">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
